d5ee2c4fb5907b65febe91e10131a54fdd90f1c2
Tuan Anh <tuan73176@gmail.com>
2026-02-01 15:34:43 +0000

    drivers/misc/mediatek/base/power/clkbuf_v1: import OEM changes
    Change-Id: I930ab340da303ad9679db7426e36e4c99483fe9c
    Signed-off-by: Tuan Anh <tuan73176@gmail.com>

--- drivers/misc/mediatek/base/power/clkbuf_v1/mt6885/mtk_clkbuf_hw.c
+++ drivers/misc/mediatek/base/power/clkbuf_v1/mt6885/mtk_clkbuf_hw.c
@@ -1163,9 +1163,6 @@ static ssize_t clk_buf_show_status_info_internal(char *buf)
 
 u8 clk_buf_get_xo_en_sta(enum xo_id id)
 {
-	if (id < 0 || id >= XO_NUMBER)
-		return 0;
-
 	clk_buf_get_xo_en();
 
 	return xo_en_stat[id];
@@ -1620,10 +1617,12 @@ void clk_buf_post_init(void)
 #endif
 #endif
 
+#ifndef CONFIG_LGE_USB_LATTICE_SLS11HC
 #ifndef CONFIG_NFC_CHIP_SUPPORT
 	/* no need to use XO_NFC if no NFC */
 	clk_buf_ctrl_internal(CLK_BUF_NFC, CLK_BUF_FORCE_OFF);
 	CLK_BUF3_STATUS = CLOCK_BUFFER_DISABLE;
+#endif
 #endif
 #ifdef CLKBUF_USE_BBLPM
 	if (bblpm_switch == 2) {
