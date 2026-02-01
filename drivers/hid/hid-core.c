e9f6c57124ec9cdf21283bdf71043e0eaa27a6da
Tuan Anh <tuan73176@gmail.com>
2026-02-01 15:34:30 +0000

    drivers/hid: import OEM changes
    Change-Id: Ibeccd7dc9fae5ebf5369a31b84b1273ee7266e81
    Signed-off-by: Tuan Anh <tuan73176@gmail.com>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -49,6 +49,13 @@ module_param_named(debug, hid_debug, int, 0600);
 MODULE_PARM_DESC(debug, "toggle HID debugging messages");
 EXPORT_SYMBOL_GPL(hid_debug);
 
+int hid_touch_debug_mask = BASE_INFO;
+/* Debug mask value
+ * usage: echo [debug_mask] > /sys/module/hid/parameters/debug_mask
+ */
+module_param_named(debug_mask, hid_touch_debug_mask, int, 0664);
+MODULE_PARM_DESC(debug_mask, "Use Touch_HID debug log trace");
+
 static int hid_ignore_special_drivers = 0;
 module_param_named(ignore_special_drivers, hid_ignore_special_drivers, int, 0600);
 MODULE_PARM_DESC(ignore_special_drivers, "Ignore any special drivers and handle all devices by generic driver");
@@ -90,7 +97,8 @@ EXPORT_SYMBOL_GPL(hid_register_report);
  * Register a new field for this report.
  */
 
-static struct hid_field *hid_register_field(struct hid_report *report, unsigned usages, unsigned values)
+static struct hid_field *hid_register_field(struct hid_report *report,
+					    unsigned int usages)
 {
 	struct hid_field *field;
 
@@ -101,7 +109,7 @@ static struct hid_field *hid_register_field(struct hid_report *report, unsigned 
 
 	field = kzalloc((sizeof(struct hid_field) +
 			 usages * sizeof(struct hid_usage) +
-			 values * sizeof(unsigned)), GFP_KERNEL);
+			 usages * sizeof(unsigned int)), GFP_KERNEL);
 	if (!field)
 		return NULL;
 
@@ -280,7 +288,7 @@ static int hid_add_field(struct hid_parser *parser, unsigned report_type, unsign
 	usages = max_t(unsigned, parser->local.usage_index,
 				 parser->global.report_count);
 
-	field = hid_register_field(report, usages, parser->global.report_count);
+	field = hid_register_field(report, usages);
 	if (!field)
 		return 0;
 
@@ -656,6 +664,8 @@ static void hid_free_report(struct hid_report *report)
 static void hid_close_report(struct hid_device *device)
 {
 	unsigned i, j;
+
+	HID_TOUCH_TRACE();
 
 	for (i = 0; i < HID_REPORT_TYPES; i++) {
 		struct hid_report_enum *report_enum = device->report_enum + i;
@@ -1333,6 +1343,8 @@ static void hid_process_event(struct hid_device *hid, struct hid_field *field,
 	struct hid_driver *hdrv = hid->driver;
 	int ret;
 
+	HID_TOUCH_TRACE();
+
 	if (!list_empty(&hid->debug_list))
 		hid_dump_input(hid, usage, value);
 
@@ -1358,6 +1370,146 @@ static void hid_process_event(struct hid_device *hid, struct hid_field *field,
  * reporting to the layer).
  */
 
+enum {
+	PEN_NONE = 0,
+	HOVER_ENTER,
+	HOVER_EXIT,
+	PRESS,
+	RELEASE,
+	SWITCH_1_P,
+	SWITCH_1_R,
+	SWITCH_2_P,
+	SWITCH_2_R,
+	DEBUG_1,
+	DEBUG_2,
+};
+
+static const char __used *pen_flow[11] = {
+	[0] = "<NONE>      ",
+	[1] = "<HoverEnter>",
+	[2] = "<HoverExit> ",
+	[3] = "<Press>     ",
+	[4] = "<Release>   ",
+	[5] = "<Button1_P> ",
+	[6] = "<Button1_R> ",
+	[7] = "<Button2_P> ",
+	[8] = "<Button2_R> ",
+	[9] = "<Debug(1)>  ",
+	[10] = "<Debug(2)> ",
+};
+
+static void hid_print_pen_log(struct hid_field *field, __s32 *value)
+{
+	unsigned n = 0;
+	unsigned count = field->report_count;
+	static u8 in_range = 0;
+	static u8 contact = 0;
+	static int x = 0;
+	static int y = 0;
+	static int pressure = 0;
+	static int tilt = 0;
+	static int azimuth = 0;
+	static int battery = 0;
+	static int pen_flow_num = PEN_NONE;
+	static int old_pen_flow_num = PEN_NONE;
+	static int swit1 = 0;
+	static int swit2 = 0;
+
+	for (n = 0; n < count; n++) {
+		switch (field->usage[n].hid) {
+			case HID_DG_TIPSWITCH:
+				if (value[n])
+					contact = 1;
+				break;
+			case HID_DG_INRANGE:
+				if (value[n])
+					in_range = 1;
+				break;
+			case HID_DG_BARRELSWITCH:
+				swit1 = (int)value[n];
+				break;
+			case HID_DG_BARRELSWITCH2:
+				swit2 = (int)value[n];
+				break;
+			case HID_GD_X:
+				x = (int)value[n];
+				break;
+			case HID_GD_Y:
+				y = (int)value[n];
+				break;
+			case HID_DG_BATTERYSTRENGTH:
+				battery = (int)value[n];
+				break;
+			case HID_DG_TIPPRESSURE:
+				pressure = (int)value[n];
+				break;
+			case HID_DG_TILT_X:
+				tilt = (int)value[n];
+				break;
+			case HID_DG_TILT_Y:
+				azimuth = (int)value[n];
+				break;
+			default :
+				break;
+		}
+
+	}
+
+	if (field->usage[count-1].hid == HID_DG_TILT_Y) {
+		if ((in_range == 0) && (contact == 0)) {
+			pen_flow_num = HOVER_EXIT;
+		} else	{
+			if((in_range == 1) && (contact == 1)) {
+				pen_flow_num = PRESS;
+			} else if((in_range == 1) && (contact == 0)) {
+				if (old_pen_flow_num == PRESS) {
+					pen_flow_num = RELEASE;
+				} else {
+					pen_flow_num = HOVER_ENTER;
+				}
+			}
+
+			if((in_range == 0) && (contact == 1)) {
+				pen_flow_num = DEBUG_1;
+			}
+		}
+
+		if (old_pen_flow_num != pen_flow_num) {
+			TOUCH_I("%s X: %4d Y: %4d Batt: %2d P: %4d T: %3d A: %3d\n",
+					pen_flow[pen_flow_num],
+					x,
+					y,
+					battery,
+					pressure,
+					tilt,
+					azimuth);
+		}
+
+		TOUCH_D(ABS, "%s in_r:%d cont:%d sw1:%d sw2:%d bat:%d x:%d y:%d z:%d tt:%d am:%d\n",
+				pen_flow[pen_flow_num],
+				in_range,
+				contact,
+				swit1,
+				swit2,
+				battery,
+				x,
+				y,
+				pressure,
+				tilt,
+				azimuth);
+
+		old_pen_flow_num = pen_flow_num;
+		pen_flow_num = PEN_NONE;
+		in_range = 0;
+		contact = 0;
+		x = 0;
+		y = 0;
+		pressure = 0;
+		tilt = 0;
+		azimuth = 0;
+		battery = 0;
+	}
+}
 static void hid_input_field(struct hid_device *hid, struct hid_field *field,
 			    __u8 *data, int interrupt)
 {
@@ -1369,6 +1521,7 @@ static void hid_input_field(struct hid_device *hid, struct hid_field *field,
 	__s32 max = field->logical_maximum;
 	__s32 *value;
 
+	HID_TOUCH_TRACE();
 	value = kmalloc(sizeof(__s32) * count, GFP_ATOMIC);
 	if (!value)
 		return;
@@ -1408,6 +1561,9 @@ static void hid_input_field(struct hid_device *hid, struct hid_field *field,
 				hid_process_event(hid, field, &field->usage[value[n] - min], 1, interrupt);
 	}
 
+	if (field->application == HID_DG_PEN)
+		hid_print_pen_log(field, value);
+
 	memcpy(field->value, value, count * sizeof(__s32));
 exit:
 	kfree(value);
@@ -1424,6 +1580,8 @@ static void hid_output_field(const struct hid_device *hid,
 	unsigned offset = field->report_offset;
 	unsigned size = field->report_size;
 	unsigned n;
+
+	HID_TOUCH_TRACE();
 
 	for (n = 0; n < count; n++) {
 		if (field->logical_minimum < 0)	/* signed values */
@@ -1454,6 +1612,8 @@ static size_t hid_compute_report_size(struct hid_report *report)
 void hid_output_report(struct hid_report *report, __u8 *data)
 {
 	unsigned n;
+
+	HID_TOUCH_TRACE();
 
 	if (report->id > 0)
 		*data++ = report->id;
@@ -1490,6 +1650,8 @@ int hid_set_field(struct hid_field *field, unsigned offset, __s32 value)
 {
 	unsigned size;
 
+	HID_TOUCH_TRACE();
+
 	if (!field)
 		return -1;
 
@@ -1519,6 +1681,8 @@ static struct hid_report *hid_get_report(struct hid_report_enum *report_enum,
 	struct hid_report *report;
 	unsigned int n = 0;	/* Normally report number is 0 */
 
+	HID_TOUCH_TRACE();
+
 	/* Device uses numbered reports, data[0] is report number */
 	if (report_enum->numbered)
 		n = *data;
@@ -1540,6 +1704,8 @@ void __hid_request(struct hid_device *hid, struct hid_report *report,
 	char *buf;
 	int ret;
 	u32 len;
+
+	HID_TOUCH_TRACE();
 
 	buf = hid_alloc_report_buf(report, GFP_KERNEL);
 	if (!buf)
@@ -1575,6 +1741,8 @@ int hid_report_raw_event(struct hid_device *hid, int type, u8 *data, u32 size,
 	u32 rsize, csize = size;
 	u8 *cdata = data;
 	int ret = 0;
+
+	HID_TOUCH_TRACE();
 
 	report = hid_get_report(report_enum, data);
 	if (!report)
@@ -1638,6 +1806,8 @@ int hid_input_report(struct hid_device *hid, int type, u8 *data, u32 size, int i
 	struct hid_driver *hdrv;
 	struct hid_report *report;
 	int ret = 0;
+
+	HID_TOUCH_TRACE();
 
 	if (!hid)
 		return -ENODEV;
@@ -1765,6 +1935,8 @@ int hid_connect(struct hid_device *hdev, unsigned int connect_mask)
 	int len;
 	int ret;
 
+	HID_TOUCH_TRACE();
+
 	if (hdev->quirks & HID_QUIRK_HIDDEV_FORCE)
 		connect_mask |= (HID_CONNECT_HIDDEV_FORCE | HID_CONNECT_HIDDEV);
 	if (hdev->quirks & HID_QUIRK_HIDINPUT_FORCE)
@@ -1849,6 +2021,8 @@ EXPORT_SYMBOL_GPL(hid_connect);
 
 void hid_disconnect(struct hid_device *hdev)
 {
+	HID_TOUCH_TRACE();
+
 	device_remove_file(&hdev->dev, &dev_attr_country);
 	if (hdev->claimed & HID_CLAIMED_INPUT)
 		hidinput_disconnect(hdev);
@@ -1872,6 +2046,8 @@ EXPORT_SYMBOL_GPL(hid_disconnect);
 int hid_hw_start(struct hid_device *hdev, unsigned int connect_mask)
 {
 	int error;
+
+	HID_TOUCH_TRACE();
 
 	error = hdev->ll_driver->start(hdev);
 	if (error)
@@ -1898,6 +2074,7 @@ EXPORT_SYMBOL_GPL(hid_hw_start);
  */
 void hid_hw_stop(struct hid_device *hdev)
 {
+	HID_TOUCH_TRACE();
 	hid_disconnect(hdev);
 	hdev->ll_driver->stop(hdev);
 }
@@ -1914,6 +2091,8 @@ EXPORT_SYMBOL_GPL(hid_hw_stop);
 int hid_hw_open(struct hid_device *hdev)
 {
 	int ret;
+
+	HID_TOUCH_TRACE();
 
 	ret = mutex_lock_killable(&hdev->ll_open_lock);
 	if (ret)
@@ -1941,6 +2120,8 @@ EXPORT_SYMBOL_GPL(hid_hw_open);
  */
 void hid_hw_close(struct hid_device *hdev)
 {
+	HID_TOUCH_TRACE();
+
 	mutex_lock(&hdev->ll_open_lock);
 	if (!--hdev->ll_open_count)
 		hdev->ll_driver->close(hdev);
@@ -2593,6 +2774,8 @@ static int hid_device_probe(struct device *dev)
 	const struct hid_device_id *id;
 	int ret = 0;
 
+	HID_TOUCH_TRACE();
+
 	if (down_interruptible(&hdev->driver_input_lock)) {
 		ret = -EINTR;
 		goto end;
@@ -2631,6 +2814,8 @@ static int hid_device_remove(struct device *dev)
 	struct hid_device *hdev = to_hid_device(dev);
 	struct hid_driver *hdrv;
 	int ret = 0;
+
+	HID_TOUCH_TRACE();
 
 	if (down_interruptible(&hdev->driver_input_lock)) {
 		ret = -EINTR;
@@ -3039,6 +3224,8 @@ int hid_add_device(struct hid_device *hdev)
 	static atomic_t id = ATOMIC_INIT(0);
 	int ret;
 
+	HID_TOUCH_TRACE();
+
 	if (WARN_ON(hdev->status & HID_STAT_ADDED))
 		return -EBUSY;
 
@@ -3107,6 +3294,8 @@ struct hid_device *hid_allocate_device(void)
 	struct hid_device *hdev;
 	int ret = -ENOMEM;
 
+	HID_TOUCH_TRACE();
+
 	hdev = kzalloc(sizeof(*hdev), GFP_KERNEL);
 	if (hdev == NULL)
 		return ERR_PTR(ret);
@@ -3130,6 +3319,8 @@ EXPORT_SYMBOL_GPL(hid_allocate_device);
 
 static void hid_remove_device(struct hid_device *hdev)
 {
+	HID_TOUCH_TRACE();
+
 	if (hdev->status & HID_STAT_ADDED) {
 		device_del(&hdev->dev);
 		hid_debug_unregister(hdev);
@@ -3150,6 +3341,7 @@ static void hid_remove_device(struct hid_device *hdev)
  */
 void hid_destroy_device(struct hid_device *hdev)
 {
+	HID_TOUCH_TRACE();
 	hid_remove_device(hdev);
 	put_device(&hdev->dev);
 }
@@ -3163,6 +3355,8 @@ int __hid_register_driver(struct hid_driver *hdrv, struct module *owner,
 	hdrv->driver.owner = owner;
 	hdrv->driver.mod_name = mod_name;
 
+	HID_TOUCH_TRACE();
+
 	INIT_LIST_HEAD(&hdrv->dyn_list);
 	spin_lock_init(&hdrv->dyn_lock);
 
@@ -3172,6 +3366,7 @@ EXPORT_SYMBOL_GPL(__hid_register_driver);
 
 void hid_unregister_driver(struct hid_driver *hdrv)
 {
+	HID_TOUCH_TRACE();
 	driver_unregister(&hdrv->driver);
 	hid_free_dynids(hdrv);
 }
@@ -3181,6 +3376,8 @@ int hid_check_keys_pressed(struct hid_device *hid)
 {
 	struct hid_input *hidinput;
 	int i;
+
+	HID_TOUCH_TRACE();
 
 	if (!(hid->claimed & HID_CLAIMED_INPUT))
 		return 0;
@@ -3199,6 +3396,8 @@ EXPORT_SYMBOL_GPL(hid_check_keys_pressed);
 static int __init hid_init(void)
 {
 	int ret;
+
+	HID_TOUCH_TRACE();
 
 	if (hid_debug)
 		pr_warn("hid_debug is now used solely for parser and driver debugging.\n"
@@ -3225,6 +3424,7 @@ err:
 
 static void __exit hid_exit(void)
 {
+	HID_TOUCH_TRACE();
 	hid_debug_exit();
 	hidraw_exit();
 	bus_unregister(&hid_bus_type);
